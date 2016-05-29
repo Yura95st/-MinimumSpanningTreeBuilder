a9be27645a21d071c2b98874212167c9feaf7c36
Yura95st <yura95st@gmail.com>
2016-05-29 07:22:04 +0300

    Initial commit

---
+++ src/minimumSpanningTreeBuilder/Main.java
@@ -0,0 +1,17 @@
+package minimumSpanningTreeBuilder;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+		}
+		catch (Exception exception)
+		{
+			System.err.println("Error occured:");
+			System.err.println(exception);
+		}
+	}
+
+}
